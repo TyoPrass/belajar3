802f9beeb28ea9c82a6834afd045ccbb2c95b898
TyoPrass <220441100072@student.trunojoyo.ac.id>
2025-08-29 15:51:53 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Artikel 1</h2>
+        <div class="text-base  text-gray-500">
+            <a href="#">Sigma Guntur</a> | 30 Juni 2025
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, iure, animi
+            facilis incidunt, corporis
+            accusantium quam delectus ab nam autem est possimus alias qui! Odio nisi dolor et voluptas molestias.</p>
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+    </article>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Artikel 2</h2>
+        <div class="text-base  text-gray-500">
+            <a href="#">Sigma Guntur</a> | 1 12 Agustus 2025
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, iure, animi
+            facilis incidunt, corporis
+            accusantium quam delectus ab nam autem est possimus alias qui! Odio nisi dolor et voluptas molestias.</p>
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+    </article>
+
+</x-layout>
