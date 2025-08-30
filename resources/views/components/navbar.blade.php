611ffd2de002e4cf398209e2f03a67175bee8fbc
TyoPrass <220441100072@student.trunojoyo.ac.id>
2025-08-30 13:49:13 +0700

    Learn About View Data

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,7 +12,7 @@
                              <!-- <a href="/" aria-current="page"
                                  class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium">Home</a> -->
                              <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                             <x-nav-link href="/blog" :active="request()->is('blog')">Blog</x-nav-link>
+                             <x-nav-link href="/posts" :active="request()->is('posts')">Blog</x-nav-link>
                              <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
                              <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
 
@@ -95,16 +95,10 @@
          <el-disclosure id="mobile-menu" hidden class="block md:hidden">
              <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                  <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                 <a href="#" aria-current="page"
-                     class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Dashboard</a>
-                 <a href="#"
-                     class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Team</a>
-                 <a href="#"
-                     class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Projects</a>
-                 <a href="#"
-                     class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Calendar</a>
-                 <a href="#"
-                     class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Reports</a>
+                 <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                 <x-nav-link href="/posts" :active="request()->is('posts')">Blog</x-nav-link>
+                 <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+                 <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
              </div>
              <div class="border-t border-white/10 pt-4 pb-3">
                  <div class="flex items-center px-5">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,9 +11,58 @@ Route::get('/about', function (){
     return view('about', ['title' => 'About']);
 });
 
-Route::get('/blog', function(){
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function(){
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [   'id' => 1,
+            'slug' => 'artikel-1',
+            'title' => 'Artikel 1',
+            'author' => 'Sigma Guntur',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, iure, animi
+            facilis incidunt, corporis
+            accusantium quam delectus ab nam autem est possimus alias qui! Odio nisi dolor et voluptas molestias.'
+    ],
+        [ 
+            'id' => 2,
+            'slug' => 'artikel-2',
+            'title' => 'Artikel 2',
+            'author' => 'Liem Kwan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet adipisci nam
+            quibusdam at cupiditate ipsa accusamus enim commodi, earum corporis facere molestiae, tempore eaque
+            necessitatibus vero id temporibus. Esse, nisi!'
+    ],
+    ]]);
 });
+
+Route::get('/posts/{id}', function($slug){
+    $posts = [
+        [   'id' => 1,
+            'slug' => 'artikel-1',
+            'title' => 'Artikel 1',
+            'author' => 'Sigma Guntur',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, iure, animi
+            facilis incidunt, corporis
+            accusantium quam delectus ab nam autem est possimus alias qui! Odio nisi dolor et voluptas molestias.'
+    ],
+        [ 
+            'id' => 2,
+            'slug' => 'artikel-2',
+            'title' => 'Artikel 2',
+            'author' => 'Liem Kwan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet adipisci nam
+            quibusdam at cupiditate ipsa accusamus enim commodi, earum corporis facere molestiae, tempore eaque
+            necessitatibus vero id temporibus. Esse, nisi!'
+    ],
+];
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+
+});
+
+
+
 
 Route::get('/contact', function(){
     return view('contact', ['title' => 'Contact']);
